6e79e7351820d12d4f6b22b3284061b3bc5920c2
amanda2513 <em3573@wayne.edu>
2013-10-13 02:39:16 -0400

    Responsive layout - Home and Registration
    Trying to make it look better on phone and tablet

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo (CSS.'bootstrap.css');?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo (CSS.'bootstrap-responsive.css');?>">
@@ -55,24 +56,29 @@
   				echo '</div>';
   			}
   		?>
-
-		<h2 class="wsu_h2 text-center">Graduate Exhibition Registration &amp; Scoring System</h2>
-			<p class="text-center">The Graduate School is pleased to invite the campus community to the third annual Graduate Exhibition to celebrate the achievements of Wayne State University graduate students across our campus. The event will include an exhibition of graduate student research and scholarship from all disciplines, and will feature 100 posters and art exhibits and six oral presentations.</p>
+  		<div class="content">
+			<h2 class="wsu_h2 text-center hidden-phone">Graduate Exhibition Registration &amp; Scoring System</h2>
+			<h4 class="wsu_h2 text-center visible-phone">Graduate Exhibition Registration &amp; Scoring System</h4>
+			<p>The Graduate School is pleased to invite the campus community to the third annual Graduate Exhibition to celebrate the achievements of Wayne State University graduate students across our campus. The event will include an exhibition of graduate student research and scholarship from all disciplines, and will feature 100 posters and art exhibits and six oral presentations.</p>
 			</br>
-			<p class="text-center">The event will be held [DATE] from [START - END] in [LOCATION]. We hope many will join us for this wonderful event!</p>
+			<p class="text-center">We hope many will join us for this wonderful event!</p></br>
+			<p class="text-center">The event will be held: </br> [DATE] from [START - END] in [LOCATION].</p>
 			</br>
-			<div class="reg_button_container">
+			<div class="reg_button_container row-fluid">
              	
 				<?php 
 					$participant="participant";
 					$judge="judge";
 				?>
 
-				<a class="btn btn-lg wsu_btn" id="participant_register_btn" href='<?php echo base_url()."gerss/registration?type=",urlencode($participant)?>'><strong>Participants</strong></br>Register Here</a>
-				<a class="btn btn-lg wsu_btn" id="judge_register_btn" href='<?php echo base_url()."gerss/registration?type=",urlencode($judge)?>'><strong>Judges</strong></br>Register Here</a>
-
+				<div class="span6">
+					<a class="btn btn-lg wsu_btn span5 offset3" id="participant_register_btn" href='<?php echo base_url()."gerss/registration?type=",urlencode($participant)?>'><strong>Participants</strong></br>Register Here</a>
+				</div>
+				<div class="span6">
+					<a class="btn btn-lg wsu_btn span5 offset3" id="judge_register_btn" href='<?php echo base_url()."gerss/registration?type=",urlencode($judge)?>'><strong>Judges</strong></br>Register Here</a>
+				</div>
 			</div>
+		</div>
 	</div>
-
 </body>
 </html>
--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -2,6 +2,7 @@
 <html>
 <head lang="en">
 	<title><?php echo $title; ?></title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="<?php echo (CSS.'bootstrap.css');?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo (CSS.'bootstrap-responsive.css');?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo (CSS.'grad-project.css');?>">
@@ -11,6 +12,7 @@
 <body>
 	<div class="page-header" id="wsu_header">
 		<a href="http://www.wayne.edu"><img id="wsu_logo" src="<?php echo (IMG.'wsu-wordmark.gif');?>"/></a>
+
 		<div class="wsu_sign_in_container pull-right">
 			<?php 
 				if(validation_errors() != false){
@@ -23,7 +25,7 @@
   			?>
 			<?php 
 				$form_attributes = 
-					array('name'=>'signinform','class'=>'form-inline pull-right','id'=>'sign_in_form');
+					array('name'=>'signinform','class'=>'form-inline','id'=>'sign_in_form');
 				echo form_open('gerss/login_validation', $form_attributes);
 
 				$username_attributes = 
@@ -41,10 +43,11 @@
 				echo form_close();
 			?>
 		</div>
-	</div>
+	</div><!--close header-->
 
 	<div class="hero-unit wsu_hero_unit">
-		<h2 class="wsu_h2 text-center">Graduate Exhibition Registration &amp; Scoring System</h2>
+		<h2 class="wsu_h2 text-center hidden-phone hidden-tablet">Graduate Exhibition Registration &amp; Scoring System</h2>
+		<h4 class="wsu_h2 text-center visible-phone visible-tablet">Graduate Exhibition Registration &amp; Scoring System</h4>
 		<!--Invalid registrations redirect back to this page with errors-->
 		<?php		
 			//Get the type of registration from the URL .../registration?type=[whateverthisis] and store it in variable selected_option
@@ -81,131 +84,137 @@
 		</form><!--end registration-type form-->
 		
 		<hr class="muted"/>
-		<div class="row-fluid">
-			<?php
-			//If there are errors print them all in a bootstrap alert div
-				if($this->session->flashdata('errors')){
-					echo '<div class="alert text-center" id="wsu_alert">';
-					echo $this->session->flashdata('errors');
-					echo '</div>';
-	  			}
-	  			//If there are no errors, show "Fill out form" message
-	  			else{
-	  				echo '<p class="span10 offset2">Fill out the form below to register:</p>';
-	  			}
-	  		?>
+		<?php
+		//If there are errors print them all in a bootstrap alert div
+			if($this->session->flashdata('errors')){
+				echo '<div class="alert text-center" id="wsu_alert">';
+				echo $this->session->flashdata('errors');
+				echo '</div>';
+  			}
+  			//If there are no errors, show "Fill out form" message
+  			else{
+  				echo '<p class="span10 offset2">Fill out the form below to register:</p>';
+  			}
+  		?>
 
-			<!--
-				Actual Registration Submission Form
-				Name, Department, Email, Password
-				Same for participants & judges until IF statement
-			-->
-			<form class="form-horizontal" name="registration" id="registration" method="post" accept-charset="utf-8" action='<?php echo base_url()."gerss/registration_validation";?>'>
-				<?php
-				//Change form layout based on registration type
-					if($selected_type=='participant'){
-						echo '<div class="span6">';
-					}
-					else{
-						echo '<div class="span6 offset3">';
-					}
-				?>
+		<!--
+			Actual Registration Submission Form
+			Name, Department, Email, Password
+			Same for participants & judges until IF statement
+		-->
+		<form class="form-horizontal" name="registration" id="registration" method="post" accept-charset="utf-8" action='<?php echo base_url()."gerss/registration_validation";?>'>
+			<div class="row-fluid">
+				<div class="span12">
+					<div class="row-fluid">
+						<?php
+						//Change form layout based on registration type
+						//start form's left column
+							if($selected_type=='participant'){
+								echo '<div class="span6">';
+							}
+							else{
+								echo '<div class="span6 offset3">';
+							}
+						?>
+							<p class="text-center muted"><small>Basic Information</small></p>
 
-				<input type="hidden" name="type" id="type" value="<?php echo $selected_type?>">
+							<input type="hidden" name="type" id="type" value="<?php echo $selected_type?>">
 
-    			<div class="control-group">
-    				<label class="control-label" for="full_name">Name:</label>
-    				<div class="controls inline" name="full_name">
-    					<input type="text" name="firstname" placeholder="First Name"/>
-    					<input type="text" name="lastname" placeholder="Last Name"/>
-    				</div>
-    			</div>
+			    			<div class="control-group">
+			    				<label class="control-label" for="full_name">Name:</label>
+			    				<div class="controls inline" name="full_name">
+			    					<input type="text" name="firstname" class="input-large" placeholder="First Name"/>
+			    					<input type="text" name="lastname" class="input-large" placeholder="Last Name"/>
+			    				</div>
+			    			</div>
 
-				<div class="control-group">
-					<label class="control-label" for="department">Department:</label>
-					<div class="controls">
-						<select name="department">
-							<option value="">Select Your Department</option>
-							<option value="Computer Science">Computer Science</option>
-							<option value="English">English</option>
-							<option value="Science">Science</option>
-						</select>
-					</div>
-				</div>
-
-
-				<div class="control-group">
-					<label class="control-label" for="email">WSU Email:</label>
-					<div class="controls">
-						<input type="email" name="email" class="input-large" id="email" placeholder="Email" action="<?php echo $this->input->post('email');?>">
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="password">Create a Password:</label>
-					<div class="controls">
-						<input type="password" name="password" class="input-large" id="password" placeholder="Password">
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="cpassword">Confirm Password:</label>
-					<div class="controls">
-						<input type="password" name="cpassword" class="input-large" id="confirm_password" placeholder="Password">
-					</div>
-				</div>
-			</div><!--close left span-->
-
-			<?php
-				//If the registration type is Participant, add these fields about their project
-				if($selected_type=='participant'){
-					echo 
-					//close other column and open new column - span6
-					'<div class="span6">';
-					echo
-						'
-						<div class="control-group">
-							<label class="control-label" for="category">Project Category:</label>
-							<div class="controls">
-								<select name="category" id="category">
-									<option value="Poster">Poster</option>
-									<option value="Oral Presentation">Oral Presentation</option>
-								</select>
+							<div class="control-group">
+								<label class="control-label" for="department">Department:</label>
+								<div class="controls">
+									<select name="department">
+										<option value="">Select Your Department</option>
+										<option value="Computer Science">Computer Science</option>
+										<option value="English">English</option>
+										<option value="Science">Science</option>
+									</select>
+								</div>
 							</div>
-						</div>
 
-						<div class="control-group">
-							<label class="control-label" for="project_title">Project Title:</label>
-							<div class="controls">
-								<input type="text" name="project_title" class="input-xlarge" id="title" placeholder="Title">
+
+							<div class="control-group">
+								<label class="control-label" for="email">WSU Email:</label>
+								<div class="controls">
+									<input type="email" name="email" class="input-large" id="email" placeholder="Email" action="<?php echo $this->input->post('email');?>">
+								</div>
 							</div>
-						</div>
-						<div class="control-group">
-							<label class="control-label" for="project_desc">Project Description:</label>
-							<div class="controls">
-								<textarea name="project_desc" class="span12" id="description" rows="6" placeholder="Abstract should not exceed 250 words"></textarea>
+
+							<div class="control-group">
+								<label class="control-label" for="password">Create a Password:</label>
+								<div class="controls">
+									<input type="password" name="password" class="input-large" id="password" placeholder="Password">
+								</div>
 							</div>
-						</div>
-					</div><!--close participant only span-->
-			
+
+							<div class="control-group">
+								<label class="control-label" for="cpassword">Confirm Password:</label>
+								<div class="controls">
+									<input type="password" name="cpassword" class="input-large" id="confirm_password" placeholder="Password">
+								</div>
+							</div>
+						</div><!--close left span-->
+
+					<?php
+						//If the registration type is Participant, add these fields about their project
+						if($selected_type=='participant'){
+							echo 
+							//open right column - span6 - participant only
+							'<div class="span6">
+
+								<p class="text-center muted"><small>Project Information</small></p>
+
+								<div class="control-group">
+									<label class="control-label" for="category">Category:</label>
+									<div class="controls">
+										<select name="category" id="category">
+											<option value="Poster">Poster</option>
+											<option value="Oral Presentation">Oral Presentation</option>
+										</select>
+									</div>
+								</div>
+
+								<div class="control-group">
+									<label class="control-label" for="project_title">Title:</label>
+									<div class="controls">
+										<input type="text" name="project_title" class="input-xlarge" id="title" placeholder="Title">
+									</div>
+								</div>
+								<div class="control-group">
+									<label class="control-label" for="project_desc">Description:</label>
+									<div class="controls">
+										<textarea name="project_desc" class="input-xlarge" id="description" rows="10" placeholder="Abstract should not exceed 250 words"></textarea>
+									</div>
+								</div>
+							</div><!--close right column (participant only) span-->
 					
-						<p class="text-center span8 offset2">
-							<em>By registering, you attest to having followed all federal, state and institutional regulatory requirements in the performance of the research/scholarship.</em>
-						</p>';
-				}
-				else{
-				//If the registration type is Judges, add these fields
-					
-				}
-			?>
-	</div><!--close main row-fluid"-->
-				<div class="row-fluid">
-					<div class="span12">
-						<input type="submit" name="registration_submit" class="btn btn-medium wsu_btn span2 offset5", id="register_btn" value="Register"/>
-					</div>
-				</div>
-			</form><!--close registration form-->
+							
+							<p class="text-center span8 offset2">
+								<em><small>By registering, you attest to having followed all federal, state and institutional regulatory requirements in the performance of the research/scholarship.</small></em>
+							</p>';
+						}
+						else{
+						//If the registration type is Judges, add these fields
+							
+						}
+					?>
+					</div><!--close form column row-fluid-->
+					<div class="row-fluid">
+						<div class="span12">
+							<input type="submit" name="registration_submit" class="btn btn-medium wsu_btn span2 offset5", id="register_btn" value="Register"/>
+						</div>
+					</div><!--close submit button row-fluid-->
+				</div><!--close span12-->
+			</div><!--close form's row-fluid-->
+		</form><!--close registration form-->
 	</div><!--close hero-unit-->
-
 </body>
 </html>
